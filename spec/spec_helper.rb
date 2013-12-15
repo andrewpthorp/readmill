0e70ee8d40a3448006852e229d2b47534f5a0e50
Andrew Thorp <andrewpthorp@gmail.com>
2013-12-14 21:50:57 -0600

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
 require 'readmill'
-require 'bundler'
+require 'rspec'
+require 'webmock/rspec'
 
-Bundler.require(:default, :test)
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:any, /api.readmill.com/)
+  end
+end
+
+def readmill_url(url)
+  "https://api.readmill.com/v2/#{url}"
+end
